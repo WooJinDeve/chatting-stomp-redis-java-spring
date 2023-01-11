f84e281a9c227538d8543f1e0074d9ed7da4bf3d
WoojinDeve <jwj060111@gmail.com>
2023-01-11 10:34:19 +0900

    init: project

---
+++ src/main/java/com/socket/websocket/config/WebSocketConfiguration.java
@@ -0,0 +1,15 @@
+package com.socket.websocket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+
+
+}
